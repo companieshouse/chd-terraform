d3d02d359719fdf7cef6dffdb61f64501b496c25
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2022-02-17 16:40:06 +0000

    Added output for frontend FQDN

--- groups/chd-infrastructure/outputs.tf
+++ groups/chd-infrastructure/outputs.tf
@@ -1,3 +1,7 @@
+output "chd_frontend_address_internal" {
+  value = aws_route53_record.chd_alb_internal.fqdn
+}
+
 output "chd_bep_address_internal" {
   value = aws_route53_record.nlb_backend.fqdn
 }
