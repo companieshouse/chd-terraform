c798c4c8abf8e8e236deee98230c26d715a4ef4f
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2022-04-25 11:15:31 +0100

    Updated route53 to use NLB address instead of ALB address. Updated output for new resource name

--- groups/chd-infrastructure/outputs.tf
+++ groups/chd-infrastructure/outputs.tf
@@ -1,5 +1,5 @@
 output "chd_frontend_address_internal" {
-  value = aws_route53_record.chd_alb_internal.fqdn
+  value = aws_route53_record.chd_frontend_internal.fqdn
 }
 
 output "chd_bep_address_internal" {
--- groups/chd-infrastructure/route53.tf
+++ groups/chd-infrastructure/route53.tf
@@ -10,14 +10,14 @@ resource "aws_route53_record" "nlb_backend" {
   }
 }
 
-resource "aws_route53_record" "chd_alb_internal" {
+resource "aws_route53_record" "chd_frontend_internal" {
   zone_id = data.aws_route53_zone.private_zone.zone_id
   name    = var.application
   type    = "A"
 
   alias {
-    name                   = module.chd_internal_alb.this_lb_dns_name
-    zone_id                = module.chd_internal_alb.this_lb_zone_id
+    name                   = module.nlb_fe_internal.this_lb_dns_name
+    zone_id                = module.nlb_fe_internal.this_lb_zone_id
     evaluate_target_health = true
   }
 }
